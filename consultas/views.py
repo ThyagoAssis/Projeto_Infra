aa858ed094f8e29900c24ef17a2861a78f4a767e
PROGRAMAÇÃO EM PYTHON 2024.4 <51129532024.4@senacrjedu.local>
2025-02-07 21:33:20 -0300

    Consulta_V2

--- consultas/views.py
+++ consultas/views.py
@@ -8,6 +8,7 @@ def consultar_ativos(request):
     ativos = Ativo.objects.all()
     form = ConsultaForm()
     
+    nome_id = request.GET.get('nome')
     unidade_id = request.GET.get('unidade')
     departamento_id = request.GET.get('departamento')
     tipo_id = request.GET.get('tipo')
@@ -17,16 +18,24 @@ def consultar_ativos(request):
 
     if unidade_id:
         ativos = ativos.filter(unidade_id=unidade_id)
-    if departamento_id:
+        return render(request, 'consultas/consulta.html', {'ativos': ativos})
+    elif departamento_id:
         ativos = ativos.filter(departamento_id=departamento_id)
-    if tipo_id:
+        return render(request, 'consultas/consulta.html', {'ativos': ativos})
+    elif tipo_id:
         ativos = ativos.filter(tipo_id=tipo_id)
-    if fabricante_id:
+        return render(request, 'consultas/consulta.html', {'ativos': ativos})
+    elif fabricante_id:
         ativos = ativos.filter(fabricante_id=fabricante_id)
-    if ip:
+        return render(request, 'consultas/consulta.html', {'ativos': ativos})
+    elif ip:
         ativos = ativos.filter(ip=ip)
-    if mac:
+        return render(request, 'consultas/consulta.html', {'ativos': ativos})
+    elif mac:
         ativos = ativos.filter(mac_address__icontains=mac)
-
-
-    return render(request, 'consultas/consulta.html', {'ativos': ativos, 'form': form})
+        return render(request, 'consultas/consulta.html', {'ativos': ativos})
+    elif mac:
+        ativos = ativos.filter(nome_id=nome_id)
+        return render(request, 'consultas/consulta.html', {'ativos': ativos})
+    else: 
+        return render(request, 'consultas/consulta.html', {'form':form})
